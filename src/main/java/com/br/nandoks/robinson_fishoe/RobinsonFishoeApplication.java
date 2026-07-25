70287f0ea9969a9990b538343f5db111b1467d44
nandoks <fernandooksman@gmail.com>
2026-07-25 18:18:04 +0200

    add cors filter to bypass security for testing purposes
    Signed-off-by: nandoks <fernandooksman@gmail.com>

--- src/main/java/com/br/nandoks/robinson_fishoe/RobinsonFishoeApplication.java
+++ src/main/java/com/br/nandoks/robinson_fishoe/RobinsonFishoeApplication.java
@@ -1,7 +1,13 @@
 package com.br.nandoks.robinson_fishoe;
 
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class RobinsonFishoeApplication {
@@ -10,4 +16,20 @@ public class RobinsonFishoeApplication {
 		SpringApplication.run(RobinsonFishoeApplication.class, args);
 	}
 
+	@Bean
+	public CorsFilter corsFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With", "Access-Control-Request" +
+						"-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token",
+				"Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow" +
+						"-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsFilter();
+	}
 }
