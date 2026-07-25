9e0bef666670e85a7aefa9bb001422e9e9b01caf
nandoks <fernandooksman@gmail.com>
2026-07-25 16:29:50 +0200

    first kickoff commit

---
+++ src/main/java/com/br/nandoks/robinson_fishoe/RobinsonFishoeApplication.java
@@ -0,0 +1,13 @@
+package com.br.nandoks.robinson_fishoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobinsonFishoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobinsonFishoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/nandoks/robinson_fishoe/RobinsonFishoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.nandoks.robinson_fishoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobinsonFishoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
